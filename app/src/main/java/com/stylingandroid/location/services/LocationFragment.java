0430250e4ee55926f9ef081c88a7c7d34ce0d340
Mark Allison <mark@stylingandroid.com>
2017-06-17 10:37:56 +0100

    Initial commit

---
+++ app/src/main/java/com/stylingandroid/location/services/LocationFragment.java
@@ -0,0 +1,101 @@
+package com.stylingandroid.location.services;
+
+import android.annotation.SuppressLint;
+import android.location.Location;
+import android.os.Bundle;
+import android.os.Looper;
+import android.support.annotation.NonNull;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+
+import java.util.Locale;
+
+import hugo.weaving.DebugLog;
+
+public class LocationFragment extends Fragment {
+    private static final String FRACTIONAL_FORMAT = "%.4f";
+    private static final String ACCURACY_FORMAT = "%.1fm";
+
+    private TextView latitudeValue;
+    private TextView longitudeValue;
+    private TextView accuracyValue;
+
+    private FusedLocationProviderClient locationProviderClient = null;
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        registerForLocationUpdates();
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_location, container, false);
+        latitudeValue = (TextView) view.findViewById(R.id.latitude_value);
+        longitudeValue = (TextView) view.findViewById(R.id.longitude_value);
+        accuracyValue = (TextView) view.findViewById(R.id.accuracy_value);
+        return view;
+    }
+
+    @Override
+    public void onStop() {
+        unregisterForLocationUpdates();
+        super.onStop();
+    }
+
+    void updatePosition(Location location) {
+        String latitudeString = createFractionString(location.getLatitude());
+        String longitudeString = createFractionString(location.getLongitude());
+        String accuracyString = createAccuracyString(location.getAccuracy());
+        latitudeValue.setText(latitudeString);
+        longitudeValue.setText(longitudeString);
+        accuracyValue.setText(accuracyString);
+    }
+
+    private String createFractionString(double fraction) {
+        return String.format(Locale.getDefault(), FRACTIONAL_FORMAT, fraction);
+    }
+
+    private String createAccuracyString(float accuracy) {
+        return String.format(Locale.getDefault(), ACCURACY_FORMAT, accuracy);
+    }
+
+    @DebugLog
+    @SuppressLint("MissingPermission")
+    void registerForLocationUpdates() {
+        locationProviderClient = getFusedLocationProviderClient();
+        locationProviderClient.requestLocationUpdates(LocationRequest.create(), locationCallback, Looper.myLooper());
+    }
+
+    @DebugLog
+    void  unregisterForLocationUpdates() {
+        if (locationProviderClient != null) {
+            locationProviderClient.removeLocationUpdates(locationCallback);
+        }
+    }
+
+    @DebugLog
+    @NonNull
+    private FusedLocationProviderClient getFusedLocationProviderClient() {
+        return LocationServices.getFusedLocationProviderClient(getActivity());
+    }
+
+    private LocationCallback locationCallback = new LocationCallback() {
+        @DebugLog
+        @Override
+        public void onLocationResult(LocationResult locationResult) {
+            super.onLocationResult(locationResult);
+            Location lastLocation = locationResult.getLastLocation();
+            updatePosition(lastLocation);
+        }
+    };
+}
